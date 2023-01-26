d4d67fb989c4f6cccbe5285ab184b25449cf8f21
Alex <123321081+Goofball554@users.noreply.github.com>
2023-01-26 12:54:00 -0500

    Add files via upload

---
+++ Multiples.py
@@ -0,0 +1,8 @@
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+
+if x % y == 0:
+    print(x, " is a multiple of ", y)
+else:
+    print(x, " is not a multiple of", y)
